bfc4b6a38211855357018233748b621a7e0475e0
Rafael <raf.csoares@gmail.com>
2019-12-19 18:51:00 -0300

    * upm-push.cmd fixes

--- HowToPublishPackage/upm-push.cmd
+++ HowToPublishPackage/upm-push.cmd
@@ -1,10 +1,21 @@
 @echo off
 
+pushd %~dp0
+set script_dir=%CD%
+
 for /f "tokens=1,2 delims=:," %%a in ('findstr "version" "%cd%\package.json"') do ( set version=%%b&goto :break1)
 :break1
 for /f "tokens=1,2 delims=:," %%a in ('findstr "name" "%cd%\package.json"') do ( set name=%%b&goto :break2)
 :break2
 
+if not defined name (
+@echo on
+goto :endBreak
+)
+if not defined version (
+@echo on
+goto :endBreak
+)
 set name=%name:"=%
 set name=%name: =%
 set version=%version:"=%
@@ -26,4 +37,5 @@ git push origin :refs/tags/%branchTag%
 git tag "%branchTag%" %name%
 git push origin %name% --tags
 
+:endBreak
 SET /p exit=Press any key to exit
