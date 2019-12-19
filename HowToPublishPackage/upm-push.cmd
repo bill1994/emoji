84be2f45137bef7496b2c6303e86635760f99bb6
Rafael <raf.csoares@gmail.com>
2019-12-19 05:43:56 -0300

    * Fixed in upm path

--- HowToPublishPackage/upm-push.cmd
+++ HowToPublishPackage/upm-push.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-for /f "tokens=1,2 delims=:," %%a in ('findstr "version" "package.json"') do ( set version=%%b&goto :break1)
+for /f "tokens=1,2 delims=:," %%a in ('findstr "version" "%cd%\package.json"') do ( set version=%%b&goto :break1)
 :break1
-for /f "tokens=1,2 delims=:," %%a in ('findstr "name" "package.json"') do ( set name=%%b&goto :break2)
+for /f "tokens=1,2 delims=:," %%a in ('findstr "name" "%cd%\package.json"') do ( set name=%%b&goto :break2)
 :break2
 
 set name=%name:"=%
