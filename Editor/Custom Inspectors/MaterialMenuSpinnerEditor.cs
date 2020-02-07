e57afbd0b8362c48321266cd9879eb6bb5f7aa9a
Rafael <raf.csoares@gmail.com>
2020-02-07 15:29:24 -0300

    * Fixed manu bugs in MaterialUI

---
+++ Editor/Custom Inspectors/MaterialMenuSpinnerEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using static MaterialUI.MaterialMenuSpinner;
+using static MaterialUI.AssetAdressEditor;
+
+namespace MaterialUI
+{
+    [CustomPropertyDrawer(typeof(MaterialDialogFrameAddress), true)]
+    public class MaterialDialogFrameAddressDrawer : GenericAssetAddressDrawer<MaterialDialogFrame>
+    {
+    }
+
+    [CustomEditor(typeof(MaterialMenuSpinner))]
+    public class MaterialMenuSpinnerEditor : BaseStyleElementEditor
+    {
+        private SerializedProperty m_SpinnerMode;
+        private SerializedProperty m_DropdownExpandPivot;
+        private SerializedProperty m_DropdownFramePivot;
+        private SerializedProperty m_DropdownFramePreferredSize;
+        private SerializedProperty m_CustomFramePrefabAddress;
+
+        private SerializedProperty OnCancelCallback;
+
+        protected override void OnEnable()
+        {
+            OnBaseEnable();
+            m_SpinnerMode = serializedObject.FindProperty("m_SpinnerMode");
+            m_DropdownExpandPivot = serializedObject.FindProperty("m_DropdownExpandPivot");
+            m_DropdownFramePivot = serializedObject.FindProperty("m_DropdownFramePivot");
+            m_DropdownFramePreferredSize = serializedObject.FindProperty("m_DropdownFramePreferredSize");
+            m_CustomFramePrefabAddress = serializedObject.FindProperty("m_CustomFramePrefabAddress");
+            OnCancelCallback = serializedObject.FindProperty("OnCancelCallback");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(m_CustomFramePrefabAddress, new GUIContent("Custom Address"));
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(m_SpinnerMode);
+            if (m_SpinnerMode.enumValueIndex != 1)
+            {
+                EditorGUI.indentLevel++;
+                LayoutStyle_PropertyField(m_DropdownExpandPivot);
+                LayoutStyle_PropertyField(m_DropdownFramePivot);
+                LayoutStyle_PropertyField(m_DropdownFramePreferredSize);
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(OnCancelCallback);
+
+            EditorGUILayout.Space();
+            DrawStyleGUIFolder();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Runtime/Scripts/Components/Pickers/MaterialMenuSpinner.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaterialUI
+{
+    public class MaterialMenuSpinner : BaseSpinner<EmptyStyleProperty>
+    {
+        #region Helper Classes
+
+        [System.Serializable]
+        public class MaterialDialogFrameAddress : ComponentPrefabAddress<MaterialDialogFrame>
+        {
+            public static explicit operator MaterialDialogFrameAddress(string s)
+            {
+                return new MaterialDialogFrameAddress() { AssetPath = s };
+            }
+        }
+
+        #endregion
+
+        #region Private Variables
+
+        [SerializeField]
+        MaterialDialogFrameAddress m_CustomFramePrefabAddress = null;
+
+        protected MaterialDialogFrame _CacheDialogFrame = null;
+        protected PrefabAddress _CachedPrefabAdress = null;
+
+        #endregion
+
+        #region Public Properties
+
+        protected PrefabAddress cachedPrefabAddress
+        {
+            get
+            {
+                if (_CachedPrefabAdress == null)
+                    _CachedPrefabAdress = (PrefabAddress)m_CustomFramePrefabAddress;
+                return _CachedPrefabAdress;
+            }
+        }
+
+        public virtual MaterialDialogFrameAddress customFramePrefabAddress
+        {
+            get
+            {
+                return m_CustomFramePrefabAddress;
+            }
+            set
+            {
+                if (m_CustomFramePrefabAddress == value)
+                    return;
+                ClearCache(false);
+                m_CustomFramePrefabAddress = value;
+                _CachedPrefabAdress = (PrefabAddress)m_CustomFramePrefabAddress;
+            }
+        }
+
+        #endregion
+
+        #region Unity Functions
+
+        protected override void OnDestroy()
+        {
+            ClearCache(true);
+            base.OnDestroy();
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            ClearCache(false);
+        }
+
+#endif
+
+        #endregion
+
+        #region Overriden Functions
+
+        protected virtual void ClearCache(bool hideIfExpanded)
+        {
+            if (_CachedPrefabAdress != null)
+            {
+                _CachedPrefabAdress.ClearCache();
+                _CachedPrefabAdress = null;
+            }
+            if (m_CustomFramePrefabAddress != null)
+                m_CustomFramePrefabAddress.ClearCache();
+            if (hideIfExpanded && IsExpanded())
+            {
+                _CacheDialogFrame.activity.destroyOnHide = true;
+                _CacheDialogFrame.Hide();
+            }
+#if UNITY_EDITOR
+            Resources.UnloadUnusedAssets();
+#endif
+        }
+
+        public override bool IsExpanded()
+        {
+            return (_CacheDialogFrame != null && _CacheDialogFrame.gameObject.activeSelf);
+        }
+
+        public override void Show()
+        {
+            var prefabAddress = cachedPrefabAddress;
+            if (prefabAddress == null)
+            {
+                if (IsExpanded())
+                    Hide();
+                return;
+            }
+
+            ShowFrameActivity(_CacheDialogFrame, prefabAddress, (dialog, isDialog) =>
+            {
+                _CacheDialogFrame = dialog;
+            });
+        }
+
+        public override void Hide()
+        {
+            if (_CacheDialogFrame != null && _CacheDialogFrame.gameObject.activeSelf)
+                _CacheDialogFrame.Hide();
+
+            HandleOnHide();
+        }
+
+        #endregion
+    }
+}
