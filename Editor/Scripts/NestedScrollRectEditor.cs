5ba37c15df922c0aceac6d9afc28b2723528b58b
Rafael <raf.csoares@gmail.com>
2021-01-08 19:12:49 -0300

    * NestedScrollRect SnapTo Functions

--- Editor/Scripts/NestedScrollRectEditor.cs
+++ Editor/Scripts/NestedScrollRectEditor.cs
@@ -14,10 +14,12 @@ namespace KyubEditor.UI
     [CustomEditor(typeof(NestedScrollRect), true)]
     public class NestedScrollRectEditor : ScrollRectEditor
     {
+        SerializedProperty m_SnapToDuration = null;
         SerializedProperty m_NestedDragActive = null;
 
         protected override void OnEnable()
         {
+            m_SnapToDuration = serializedObject.FindProperty("m_SnapToDuration");
             m_NestedDragActive = serializedObject.FindProperty("m_NestedDragActive");
             base.OnEnable();
         }
@@ -25,6 +27,7 @@ namespace KyubEditor.UI
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
+            EditorGUILayout.PropertyField(m_SnapToDuration);
             EditorGUILayout.PropertyField(m_NestedDragActive);
             serializedObject.ApplyModifiedProperties();
             EditorGUILayout.Space();
