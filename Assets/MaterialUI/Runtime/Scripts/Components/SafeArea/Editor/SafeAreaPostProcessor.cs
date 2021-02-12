7ba904252203b9285d22e76997aa5080be43a445
Rafael <raf.csoares@gmail.com>
2021-02-11 22:52:12 -0300

    * Fixed minor bugs

---
+++ Assets/MaterialUI/Runtime/Scripts/Components/SafeArea/Editor/SafeAreaPostProcessor.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_IPHONE && UNITY_EDITOR
+
+using System.IO;
+using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.Text;
+using UnityEditor.iOS.Xcode.Extensions;
+using System.Collections.Generic;
+
+
+//Prevent iOS 13 DarkMode to change StatusBar Text Color
+namespace KyubEditor.Credentials
+{
+    public class SafeAreaPostProcessor
+    {
+		const string KEY_UIUSER = "UIUserInterfaceStyle";
+        static string KEY_UIUSER_DESCRIPTION = "Light";
+
+        [PostProcessBuild]
+        public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+ 
+            if (buildTarget == BuildTarget.iOS) 
+            {
+                // Get plist
+                string plistPath = pathToBuiltProject + "/Info.plist";
+                PlistDocument plist = new PlistDocument();
+                plist.ReadFromString(File.ReadAllText(plistPath));
+       
+                // Get root
+                PlistElementDict rootDict = plist.root;
+
+                // Change UIUserInterfaceStyle Type
+                rootDict.SetString(KEY_UIUSER, KEY_UIUSER_DESCRIPTION);
+       
+                // Write to file
+                File.WriteAllText(plistPath, plist.WriteToString());
+            }
+        }
+    }
+}
+
+#endif
