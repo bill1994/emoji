52fa7aaa1947b7ba1371c43d3a90ad6a54a6cefb
Rafael <raf.csoares@gmail.com>
2021-02-25 02:51:39 -0300

    * UICommons Geaphics

---
+++ Editor/Scripts/SpriteImageEditor.cs
@@ -0,0 +1,41 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+using Kyub.UI;
+
+namespace KyubEditor.UI
+{
+    [CustomEditor(typeof(SpriteImage), true)]
+    [CanEditMultipleObjects]
+    public class SpriteImageEditor : ImageEditor
+    {
+        SerializedProperty m_PreserveAspect;
+        SerializedProperty m_PreserveAspectMode;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            m_PreserveAspect = serializedObject.FindProperty("m_PreserveAspect");
+            m_PreserveAspectMode = serializedObject.FindProperty("m_PreserveAspectMode");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            //EditorGUILayout.PropertyField(m_PreserveAspect);
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(m_PreserveAspectMode);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+
+#endif
