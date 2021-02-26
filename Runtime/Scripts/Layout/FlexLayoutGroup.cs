0ee8eca8a968d9eef35452a626bf8e2f227d420f
Rafael <raf.csoares@gmail.com>
2021-02-26 01:20:39 -0300

    * Fixed bugs in FlexLayouts and Improvements in SpriteImage and TextureImage

--- Runtime/Scripts/Layout/FlexLayoutGroup.cs
+++ Runtime/Scripts/Layout/FlexLayoutGroup.cs
@@ -211,7 +211,7 @@ namespace Kyub.UI
                 childSize += flexible * itemFlexibleMultiplier;
 
                 //Fit in current group
-                if (pos + childSize <= innerSize)
+                if (pos + childSize <= (startPos + innerSize))
                 {
                     currentGroup.RectChildren.Add(child);
                     pos += childSize * scaleFactor + spacing;
@@ -270,11 +270,12 @@ namespace Kyub.UI
                 LinearGroup group = _Groups[i];
                 CalcAlongGroupAxis(axis, isVertical, group);
 
-                float min = group.GetTotalMinSize(axis);
+                //float min = group.GetTotalMinSize(axis);
                 float preferred = group.GetTotalPreferredSize(axis);
                 float flexible = group.GetTotalFlexibleSize(axis);
 
-                totalMin += min + axisSpacing;
+                //TODO: FIX MIN SIZE - We removed TotalMin size as a bug found in our logic
+                //totalMin += min + axisSpacing;
                 totalPreferred += preferred + axisSpacing;
 
                 // Increment flexible size with element's flexible size.
@@ -283,7 +284,7 @@ namespace Kyub.UI
 
             if (!alongOtherAxis && rectChildren.Count > 0)
             {
-                totalMin -= axisSpacing;
+                //totalMin -= axisSpacing;
                 totalPreferred -= axisSpacing;
             }
             totalPreferred = Mathf.Max(totalMin, totalPreferred);
@@ -356,7 +357,7 @@ namespace Kyub.UI
             float pos = (axis == 0 ? padding.left : padding.top);
             float surplusSpace = size - GetTotalPreferredSize(axis);
 
-            if (surplusSpace > 0)
+            if (surplusSpace > 0 && alongOtherAxis)
             {
                 if (GetTotalFlexibleSize(axis) == 0)
                 {
@@ -427,7 +428,9 @@ namespace Kyub.UI
                     if (group.GetTotalFlexibleSize(axis) == 0)
                     {
                         if (!controlSize || !childForceExpandSize)
+                        {
                             pos += group.GetStartOffset(axis, group.GetTotalPreferredSize(axis), alignmentOnAxis);
+                        }
                         else
                             useFlexibleSpacing = true;
                     }
