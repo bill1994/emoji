d0328a11558a59e8e9815c85c2c2286188bd5995
Rafael <raf.csoares@gmail.com>
2021-05-13 01:25:48 -0300

    * Dropdown TextValidator and DateTextValidators

--- Assets/MaterialUI/Runtime/Scripts/Components/Spinners/MaterialMultiDropdown.cs
+++ Assets/MaterialUI/Runtime/Scripts/Components/Spinners/MaterialMultiDropdown.cs
@@ -75,6 +75,34 @@ namespace MaterialUI
 
         #region Properties
 
+        public MaterialInputField inputField
+        {
+            get
+            {
+                var inputField = GetComponentInChildren<MaterialInputField>(true);
+                return inputField;
+            }
+        }
+
+        public ITextValidator customTextValidator
+        {
+            get
+            {
+                return inputField != null ? inputField.customTextValidator : null;
+            }
+            set
+            {
+                if (inputField != null)
+                    inputField.customTextValidator = value;
+#if UNITY_EDITOR
+                else
+                {
+                    Debug.LogWarning("[MaterialMultiDropdown] CustomTextValidator can only be used when multi-dropdown contains an inputfield");
+                }
+#endif
+            }
+        }
+
         protected PrefabAddress cachedPrefabAddress
         {
             get
