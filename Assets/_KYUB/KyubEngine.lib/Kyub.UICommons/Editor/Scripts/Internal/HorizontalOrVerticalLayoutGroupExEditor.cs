2b5388874065889c4c68c731384189e476550e28
Rafael <raf.csoares@gmail.com>
2021-09-14 22:37:09 -0300

    * Improvements in Unity 2020.3

--- Assets/_KYUB/KyubEngine.lib/Kyub.UICommons/Editor/Scripts/Internal/HorizontalOrVerticalLayoutGroupExEditor.cs
+++ Assets/_KYUB/KyubEngine.lib/Kyub.UICommons/Editor/Scripts/Internal/HorizontalOrVerticalLayoutGroupExEditor.cs
@@ -1,46 +1,94 @@
 ﻿#if UNITY_EDITOR
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
+using UnityEditorInternal;
+using UnityEditor.AnimatedValues;
 using UnityEditor.UI;
 using Kyub.UI;
+using UnityEditor;
 
 namespace KyubEditor.UI
 {
-    [CanEditMultipleObjects]
     [CustomEditor(typeof(HorizontalOrVerticalLayoutGroupEx), true)]
-    public class HorizontalOrVerticalLayoutGroupExEditor : HorizontalOrVerticalLayoutGroupEditor
+    [CanEditMultipleObjects]
+    /// <summary>
+    /// Custom Editor for the HorizontalOrVerticalLayoutGroupEditor Component.
+    /// Extend this class to write a custom editor for a component derived from HorizontalOrVerticalLayoutGroupEditor.
+    /// </summary>
+    public class HorizontalOrVerticalLayoutGroupExEditor : Editor
     {
-        SerializedProperty m_ReverseOrder = null;
-        SerializedProperty m_ForceExpandMode = null;
+        SerializedProperty m_Padding;
+        SerializedProperty m_Spacing;
+        SerializedProperty m_ChildAlignment;
+        SerializedProperty m_ChildControlWidth;
+        SerializedProperty m_ChildControlHeight;
+        SerializedProperty m_ChildScaleWidth;
+        SerializedProperty m_ChildScaleHeight;
+        SerializedProperty m_ChildForceExpandWidth;
+        SerializedProperty m_ChildForceExpandHeight;
+        SerializedProperty m_ReverseOrder;
+        SerializedProperty m_ForceExpandMode;
 
         SerializedProperty m_InnerAlign;
         SerializedProperty m_MaxInnerWidth;
         SerializedProperty m_MaxInnerHeight;
 
-        protected override void OnEnable()
+        protected virtual void OnEnable()
         {
             m_InnerAlign = serializedObject.FindProperty("m_InnerAlign");
             m_MaxInnerWidth = serializedObject.FindProperty("m_MaxInnerWidth");
             m_MaxInnerHeight = serializedObject.FindProperty("m_MaxInnerHeight");
 
+            m_Padding = serializedObject.FindProperty("m_Padding");
+            m_Spacing = serializedObject.FindProperty("m_Spacing");
+            m_ChildAlignment = serializedObject.FindProperty("m_ChildAlignment");
+            m_ChildControlWidth = serializedObject.FindProperty("m_ChildControlWidth");
+            m_ChildControlHeight = serializedObject.FindProperty("m_ChildControlHeight");
+            m_ChildScaleWidth = serializedObject.FindProperty("m_ChildScaleWidth");
+            m_ChildScaleHeight = serializedObject.FindProperty("m_ChildScaleHeight");
+            m_ChildForceExpandWidth = serializedObject.FindProperty("m_ChildForceExpandWidth");
+            m_ChildForceExpandHeight = serializedObject.FindProperty("m_ChildForceExpandHeight");
             m_ReverseOrder = serializedObject.FindProperty("m_ReverseOrder");
             m_ForceExpandMode = serializedObject.FindProperty("m_ForceExpandMode");
-            base.OnEnable();
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
             EditorGUILayout.PropertyField(m_ReverseOrder);
-            serializedObject.ApplyModifiedProperties();
             EditorGUILayout.Space();
-            base.OnInspectorGUI();
-            serializedObject.Update();
+            EditorGUILayout.PropertyField(m_Padding, true);
+            EditorGUILayout.PropertyField(m_Spacing, true);
+            EditorGUILayout.PropertyField(m_ChildAlignment, true);
+
             EditorGUILayout.PropertyField(m_ForceExpandMode);
+            Rect rect = EditorGUILayout.GetControlRect();
+            rect = EditorGUI.PrefixLabel(rect, -1, EditorGUIUtility.TrTextContent("Control Child Size"));
+            rect.width = Mathf.Max(50, (rect.width - 4) / 3);
+            EditorGUIUtility.labelWidth = 50;
+            ToggleLeft(rect, m_ChildControlWidth, EditorGUIUtility.TrTextContent("Width"));
+            rect.x += rect.width + 2;
+            ToggleLeft(rect, m_ChildControlHeight, EditorGUIUtility.TrTextContent("Height"));
+            EditorGUIUtility.labelWidth = 0;
+
+            rect = EditorGUILayout.GetControlRect();
+            rect = EditorGUI.PrefixLabel(rect, -1, EditorGUIUtility.TrTextContent("Use Child Scale"));
+            rect.width = Mathf.Max(50, (rect.width - 4) / 3);
+            EditorGUIUtility.labelWidth = 50;
+            ToggleLeft(rect, m_ChildScaleWidth, EditorGUIUtility.TrTextContent("Width"));
+            rect.x += rect.width + 2;
+            ToggleLeft(rect, m_ChildScaleHeight, EditorGUIUtility.TrTextContent("Height"));
+            EditorGUIUtility.labelWidth = 0;
+
+            rect = EditorGUILayout.GetControlRect();
+            rect = EditorGUI.PrefixLabel(rect, -1, EditorGUIUtility.TrTextContent("Child Force Expand"));
+            rect.width = Mathf.Max(50, (rect.width - 4) / 3);
+            EditorGUIUtility.labelWidth = 50;
+            ToggleLeft(rect, m_ChildForceExpandWidth, EditorGUIUtility.TrTextContent("Width"));
+            rect.x += rect.width + 2;
+            ToggleLeft(rect, m_ChildForceExpandHeight, EditorGUIUtility.TrTextContent("Height"));
+            EditorGUIUtility.labelWidth = 0;
 
             EditorGUILayout.Space();
             m_InnerAlign.isExpanded = EditorGUILayout.Foldout(m_InnerAlign.isExpanded, "Inner Layout Options", true);
@@ -56,6 +104,23 @@ namespace KyubEditor.UI
 
             serializedObject.ApplyModifiedProperties();
         }
+
+        void ToggleLeft(Rect position, SerializedProperty property, GUIContent label)
+        {
+            bool toggle = property.boolValue;
+            EditorGUI.showMixedValue = property.hasMultipleDifferentValues;
+            EditorGUI.BeginChangeCheck();
+            int oldIndent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            toggle = EditorGUI.ToggleLeft(position, label, toggle);
+            EditorGUI.indentLevel = oldIndent;
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.boolValue = property.hasMultipleDifferentValues ? true : !property.boolValue;
+            }
+            EditorGUI.showMixedValue = false;
+        }
+
         protected void LayoutElementField(SerializedProperty property, float defaultValue)
         {
             LayoutElementField(property, _ => defaultValue);
