c852507bb247ef5ca7415fbbee26b9e6b7d44c9a
Rafael <raf.csoares@gmail.com>
2021-09-27 22:34:44 -0300

    * Generic Notification Improvements

---
+++ Assets/_KYUB/_Assets/LocalNotificationAPI/Runtime/Scripts/Generic/GenericCrossLocalNotification.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Unity.Notifications.iOS;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Kyub.LocalNotification.Generic
+{
+    /// <summary>
+    /// Generic implementation of <see cref="ICrossLocalNotification"/>.
+    /// </summary>
+    public class GenericCrossLocalNotification : ICrossLocalNotification
+    {
+        /// <inheritdoc />
+        public int? Id
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc />
+        public string Title { get; set; }
+
+        /// <inheritdoc />
+        public string Body { get; set; }
+
+        /// <inheritdoc />
+        public string Subtitle { get; set; }
+
+        /// <inheritdoc />
+        public string Data { get; set; }
+
+        /// <inheritdoc />
+        public string Group { get => CategoryIdentifier; set => CategoryIdentifier = value; }
+
+        /// <inheritdoc />
+        public int? BadgeNumber
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc />
+        public bool ShouldAutoCancel { get; set; }
+
+        /// <inheritdoc />
+        public bool Scheduled { get; private set; }
+
+        /// <inheritdoc />
+        public DateTime? DeliveryTime
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The category identifier for this notification.
+        /// </summary>
+        public string CategoryIdentifier
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Does nothing on iOS.
+        /// </summary>
+        public string SmallIcon { get => null; set {} }
+
+        /// <summary>
+        /// Does nothing on iOS.
+        /// </summary>
+        public string LargeIcon { get => null; set {} }
+
+        /// <summary>
+        /// Instantiate a new instance of <see cref="iOSCrossLocalNotification"/>.
+        /// </summary>
+        public GenericCrossLocalNotification()
+        {
+        }
+
+        /// <summary>
+        /// Mark this notifications scheduled flag.
+        /// </summary>
+        internal void OnScheduled()
+        {
+            Assert.IsFalse(Scheduled);
+            Scheduled = true;
+        }
+    }
+}
---
+++ Assets/_KYUB/_Assets/LocalNotificationAPI/Runtime/Scripts/Generic/GenericNotificationsPlatform.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Kyub.LocalNotification.Generic
+{
+    public class GenericNotificationsPlatform : ICrossLocalNotificationsPlatform<GenericCrossLocalNotification>,
+        IDisposable
+    {
+        GenericCrossLocalNotification _lastReceivedNotification = null;
+
+        List<GenericCrossLocalNotification> _scheduledNotifications = new List<GenericCrossLocalNotification>();
+
+        public event Action<ICrossLocalNotification> NotificationReceived;
+
+        public GenericNotificationsPlatform()
+        {
+            ApplicationContext.OnUpdate -= OnUpdate;
+            ApplicationContext.OnUpdate += OnUpdate;
+        }
+
+        public void CancelAllScheduledNotifications()
+        {
+            _scheduledNotifications.Clear();
+        }
+
+        public void CancelNotification(int notificationId)
+        {
+            var index = _scheduledNotifications.FindIndex(a => a != null && a.Id == notificationId);
+            if(index >= 0)
+                _scheduledNotifications.RemoveAt(index);
+        }
+
+        public GenericCrossLocalNotification CreateNotification()
+        {
+            var notification = new GenericCrossLocalNotification();
+            notification.Id = notification.GetHashCode();
+
+            return notification;
+        }
+
+        public void DismissAllDisplayedNotifications()
+        {
+        }
+
+        public void DismissNotification(int notificationId)
+        {
+        }
+
+        public void Dispose()
+        {
+            Kyub.ApplicationContext.OnUpdate -= OnUpdate;
+        }
+
+        public GenericCrossLocalNotification GetLastNotification()
+        {
+            return _lastReceivedNotification as GenericCrossLocalNotification;
+        }
+
+        public void OnBackground()
+        {
+        }
+
+        public void OnForeground()
+        {
+        }
+
+        public void OnUpdate()
+        {
+            if (_scheduledNotifications != null && _scheduledNotifications.Count > 0)
+            {
+                var notifications = _scheduledNotifications[0];
+
+                if (notifications == null || notifications.DeliveryTime == null || notifications.DeliveryTime >= DateTime.Now)
+                {
+                    _scheduledNotifications.RemoveAt(0);
+                    NotificationReceived?.Invoke(notifications);
+                }
+            }
+        }
+
+        public void ScheduleNotification(GenericCrossLocalNotification notification)
+        {
+            if (notification == null)
+                return;
+
+            if (notification.Id == null)
+                notification.Id = notification.GetHashCode();
+
+            var index = _scheduledNotifications.FindIndex(a => a != null && a.Id == notification.Id);
+            if (index >= 0)
+                _scheduledNotifications[index] = notification;
+            else
+            {
+                if (notification.DeliveryTime == null)
+                    _scheduledNotifications.Add(notification);
+
+                int addIndex = 0;
+                for (int i = 0; i < _scheduledNotifications.Count; i++)
+                {
+                    if (_scheduledNotifications[i] != null && _scheduledNotifications[i].DeliveryTime != null && _scheduledNotifications[i].DeliveryTime.Value <= notification.DeliveryTime.Value)
+                        addIndex++;
+                    else
+                    {
+                        break;
+                    }
+                }
+                if(addIndex >= _scheduledNotifications.Count)
+                    _scheduledNotifications.Add(notification);
+                else
+                    _scheduledNotifications.Insert(addIndex, notification);
+
+                notification?.OnScheduled();
+            }
+        }
+
+        public void ScheduleNotification(ICrossLocalNotification gameNotification)
+        {
+            ScheduleNotification(gameNotification as GenericCrossLocalNotification);
+        }
+
+        ICrossLocalNotification ICrossLocalNotificationsPlatform.CreateNotification()
+        {
+            return CreateNotification();
+        }
+
+        ICrossLocalNotification ICrossLocalNotificationsPlatform.GetLastNotification()
+        {
+            return GetLastNotification();
+        }
+    }
+}
