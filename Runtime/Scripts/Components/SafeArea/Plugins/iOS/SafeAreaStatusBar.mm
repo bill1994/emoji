d8a47342e90e09bbb817515ce8607954d947a818
Rafael <raf.csoares@gmail.com>
2021-12-02 17:18:39 -0300

    * Fixed crashs in iOS < 13

--- Runtime/Scripts/Components/SafeArea/Plugins/iOS/SafeAreaStatusBar.mm
+++ Runtime/Scripts/Components/SafeArea/Plugins/iOS/SafeAreaStatusBar.mm
@@ -10,13 +10,16 @@ extern "C"
 	
 	float _GetStatusBarHeight()
 	{
-#ifdef __IPHONE_13_0
-		UIStatusBarManager *manager = [UIApplication sharedApplication].keyWindow.windowScene.statusBarManager;
-		CGFloat height = manager.statusBarFrame.size.height;
-		return height;
-#else
-		CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
-		return MIN(statusBarSize.width, statusBarSize.height);
-#endif
+		if(@available(iOS 13.0, *))
+		{
+			UIStatusBarManager *manager = [UIApplication sharedApplication].keyWindow.windowScene.statusBarManager;
+			CGFloat height = manager.statusBarFrame.size.height;
+			return height;
+		}
+		else
+		{
+			CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
+			return MIN(statusBarSize.width, statusBarSize.height);
+		}
 	}
 }
