4b64ba28961a31bc885b0fc8019531eda0068653
raf.csoares@gmail.com <raf.csoares@gmail.com>
2022-03-31 22:10:45 -0300

    * Fixed primitive serializer to support string/null as number or bool

--- KSerializer/Source/Converters/PrimitiveConverter.cs
+++ KSerializer/Source/Converters/PrimitiveConverter.cs
@@ -66,14 +66,17 @@ namespace Kyub.Serialization.Internal {
             return Result.Fail("Unhandled primitive type " + instance.GetType());
         }
 
-        public override Result TryDeserialize(JsonObject storage, ref object instance, Type storageType) {
+        public override Result TryDeserialize(JsonObject storage, ref object instance, Type storageType) 
+        {
             var result = Result.Success;
 
-            if (UseBool(storageType)) {
+            if (UseBool(storageType)) 
+            {
                 var sucess = CheckType(storage, JsonObjectType.Boolean);
 
                 //(result += CheckType(storage, DataType.Boolean)).Succeeded) {
-                if (sucess.Succeeded) { 
+                if (sucess.Succeeded) 
+                { 
                     instance = storage.AsBool;
                 }
                 else if (storage.IsInt64)
@@ -81,24 +84,74 @@ namespace Kyub.Serialization.Internal {
                     sucess = Result.Success;
                     instance = ((long)Convert.ChangeType(storage.AsInt64, storageType)) != 0;
                 }
+                else if (storage.IsString && !string.IsNullOrEmpty(storage.AsString))
+                {
+                    long int64Value;
+                    bool boolValue;
+
+                    if (long.TryParse(storage.AsString, out int64Value))
+                    {
+                        sucess = Result.Success;
+                        instance = ((long)Convert.ChangeType(int64Value, storageType)) != 0;
+                    }
+                    else if (bool.TryParse(storage.AsString, out boolValue))
+                    {
+                        sucess = Result.Success;
+                        instance = boolValue;
+                    }
+                    else
+                    {
+                        sucess = Result.Success;
+                        instance = false;
+                    }
+                }
+                else
+                {
+                    sucess = Result.Success;
+                    instance = false;
+                }
                 result += sucess;
                 return result;
             }
 
-            if (UseDouble(storageType) || UseInt64(storageType)) {
-                if (storage.IsDouble) {
+            if (UseDouble(storageType) || UseInt64(storageType)) 
+            {
+                if (storage.IsDouble) 
+                {
                     instance = Convert.ChangeType(storage.AsDouble, storageType);
                 }
-                else if (storage.IsInt64) {
+                else if (storage.IsInt64) 
+                {
                     instance = Convert.ChangeType(storage.AsInt64, storageType);
                 }
-                else {
-                    return Result.Fail(GetType().Name + " expected number but got " + storage.Type + " in " + storage);
+                else if (storage.IsString)
+                {
+                    long int64Value;
+                    double doubleValue;
+
+                    if (long.TryParse(storage.AsString, out int64Value))
+                    {
+                        instance = Convert.ChangeType(int64Value, storageType);
+                    }
+                    else if (double.TryParse(storage.AsString, out doubleValue))
+                    {
+                        instance = Convert.ChangeType(doubleValue, storageType);
+                    }
+                    else
+                    {
+                        instance = Convert.ChangeType(0L, storageType);
+                    }
+                }
+                else 
+                {
+                    instance = Convert.ChangeType(0L, storageType);
+                    //return Result.Fail(GetType().Name + " expected number but got " + storage.Type + " in " + storage);
                 }
                 return Result.Success;
             }
 
-            if (UseString(storageType)) {
+            if (UseString(storageType)) 
+            {
                 if ((result += CheckType(storage, JsonObjectType.String)).Succeeded) {
                     instance = storage.AsString;
                 }
