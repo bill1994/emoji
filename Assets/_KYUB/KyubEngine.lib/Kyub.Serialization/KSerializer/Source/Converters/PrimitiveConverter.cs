c24482bc6917740fa1accdf9d399250ff97301f5
raf.csoares@gmail.com <raf.csoares@gmail.com>
2022-04-01 16:18:12 -0300

    * Fixed invalid cast

--- Assets/_KYUB/KyubEngine.lib/Kyub.Serialization/KSerializer/Source/Converters/PrimitiveConverter.cs
+++ Assets/_KYUB/KyubEngine.lib/Kyub.Serialization/KSerializer/Source/Converters/PrimitiveConverter.cs
@@ -1,63 +1,77 @@
 ﻿using System;
 
-namespace Kyub.Serialization.Internal {
-    public class PrimitiveConverter : Converter {
-        public override bool CanProcess(Type type) {
+namespace Kyub.Serialization.Internal
+{
+    public class PrimitiveConverter : Converter
+    {
+        public override bool CanProcess(Type type)
+        {
             return
                 type.Resolve().IsPrimitive ||
                 type == typeof(string) ||
                 type == typeof(decimal);
         }
 
-        public override bool RequestCycleSupport(Type storageType) {
+        public override bool RequestCycleSupport(Type storageType)
+        {
             return false;
         }
 
-        public override bool RequestInheritanceSupport(Type storageType) {
+        public override bool RequestInheritanceSupport(Type storageType)
+        {
             return false;
         }
 
-        private static bool UseBool(Type type) {
+        private static bool UseBool(Type type)
+        {
             return type == typeof(bool);
         }
 
-        private static bool UseInt64(Type type) {
+        private static bool UseInt64(Type type)
+        {
             return type == typeof(sbyte) || type == typeof(byte) ||
                    type == typeof(Int16) || type == typeof(UInt16) ||
                    type == typeof(Int32) || type == typeof(UInt32) ||
                    type == typeof(Int64) || type == typeof(UInt64);
         }
 
-        private static bool UseDouble(Type type) {
+        private static bool UseDouble(Type type)
+        {
             return type == typeof(float) ||
                    type == typeof(double) ||
                    type == typeof(decimal);
         }
 
-        private static bool UseString(Type type) {
+        private static bool UseString(Type type)
+        {
             return type == typeof(string) ||
                    type == typeof(char);
         }
 
-        public override Result TrySerialize(object instance, out JsonObject serialized, Type storageType) {
+        public override Result TrySerialize(object instance, out JsonObject serialized, Type storageType)
+        {
             var instanceType = instance.GetType();
 
-            if (UseBool(instanceType)) {
+            if (UseBool(instanceType))
+            {
                 serialized = new JsonObject((bool)instance);
                 return Result.Success;
             }
 
-            if (UseInt64(instanceType)) {
+            if (UseInt64(instanceType))
+            {
                 serialized = new JsonObject((Int64)Convert.ChangeType(instance, typeof(Int64)));
                 return Result.Success;
             }
 
-            if (UseDouble(instanceType)) {
+            if (UseDouble(instanceType))
+            {
                 serialized = new JsonObject((double)Convert.ChangeType(instance, typeof(double)));
                 return Result.Success;
             }
 
-            if (UseString(instanceType)) {
+            if (UseString(instanceType))
+            {
                 serialized = new JsonObject((string)Convert.ChangeType(instance, typeof(string)));
                 return Result.Success;
             }
@@ -66,23 +80,23 @@ namespace Kyub.Serialization.Internal {
             return Result.Fail("Unhandled primitive type " + instance.GetType());
         }
 
-        public override Result TryDeserialize(JsonObject storage, ref object instance, Type storageType) 
+        public override Result TryDeserialize(JsonObject storage, ref object instance, Type storageType)
         {
             var result = Result.Success;
 
-            if (UseBool(storageType)) 
+            if (UseBool(storageType))
             {
                 var sucess = CheckType(storage, JsonObjectType.Boolean);
 
                 //(result += CheckType(storage, DataType.Boolean)).Succeeded) {
-                if (sucess.Succeeded) 
-                { 
+                if (sucess.Succeeded)
+                {
                     instance = storage.AsBool;
                 }
                 else if (storage.IsInt64)
                 {
                     sucess = Result.Success;
-                    instance = ((long)Convert.ChangeType(storage.AsInt64, storageType)) != 0;
+                    instance = storage.AsInt64 != 0;
                 }
                 else if (storage.IsString && !string.IsNullOrEmpty(storage.AsString))
                 {
@@ -92,7 +106,7 @@ namespace Kyub.Serialization.Internal {
                     if (long.TryParse(storage.AsString, out int64Value))
                     {
                         sucess = Result.Success;
-                        instance = ((long)Convert.ChangeType(int64Value, storageType)) != 0;
+                        instance = int64Value != 0;
                     }
                     else if (bool.TryParse(storage.AsString, out boolValue))
                     {
@@ -114,13 +128,13 @@ namespace Kyub.Serialization.Internal {
                 return result;
             }
 
-            if (UseDouble(storageType) || UseInt64(storageType)) 
+            if (UseDouble(storageType) || UseInt64(storageType))
             {
-                if (storage.IsDouble) 
+                if (storage.IsDouble)
                 {
                     instance = Convert.ChangeType(storage.AsDouble, storageType);
                 }
-                else if (storage.IsInt64) 
+                else if (storage.IsInt64)
                 {
                     instance = Convert.ChangeType(storage.AsInt64, storageType);
                 }
@@ -142,7 +156,7 @@ namespace Kyub.Serialization.Internal {
                         instance = Convert.ChangeType(0L, storageType);
                     }
                 }
-                else 
+                else
                 {
                     instance = Convert.ChangeType(0L, storageType);
                     //return Result.Fail(GetType().Name + " expected number but got " + storage.Type + " in " + storage);
@@ -150,9 +164,10 @@ namespace Kyub.Serialization.Internal {
                 return Result.Success;
             }
 
-            if (UseString(storageType)) 
+            if (UseString(storageType))
             {
-                if ((result += CheckType(storage, JsonObjectType.String)).Succeeded) {
+                if ((result += CheckType(storage, JsonObjectType.String)).Succeeded)
+                {
                     instance = storage.AsString;
                 }
                 return result;
