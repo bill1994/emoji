dc246b0ce1c222447ffcc546edd21ff9119701be
raf.csoares@gmail.com <raf.csoares@gmail.com>
2022-04-12 01:58:26 -0300

    * Fixed wakeup bugs with layout

--- Assets/Kyub.UICommons.Experimental/Runtime/Scripts/Internal/FastLayoutGroup.cs
+++ Assets/Kyub.UICommons.Experimental/Runtime/Scripts/Internal/FastLayoutGroup.cs
@@ -35,7 +35,7 @@ namespace Kyub.UI.Experimental
         protected List<IFastLayoutFeedback> children { get { return m_Children; } }
 
         protected HashSet<IFastLayoutFeedback> _dirtyChildren = new HashSet<IFastLayoutFeedback>();
-        protected DrivenAxis _dirtyAxis = DrivenAxis.None;
+        protected DrivenAxis _dirtyAxis = (DrivenAxis)(-1);
 
         protected float _cachedRectHeight = -1;
         protected float _cachedRectWidth = -1;
