51e0e6eca605882738171f5a4e3c78dd07e92d3f
raf.csoares@gmail.com <raf.csoares@gmail.com>
2022-05-09 23:06:43 -0300

    * Filed errors while picking multiple files in editor

---
+++ Assets/_KYUB/_Assets/PickerServicesAPI/_Packages/StandaloneFileBrowser/Plugins/Editor/ShellFileDialogs/Native/FileSaveDialogRCW.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ShellFileDialogs
+{
+	[ComImport]
+	[ClassInterface( ClassInterfaceType.None )]
+	[Guid( ShellCLSIDGuid.FileSaveDialog )]
+	internal class FileSaveDialogRCW
+	{
+	}
+}
